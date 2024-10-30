878a427b52b0a9b96aa5d56ced65abf686001f05
Newan Kumanayaka <94077976+newan0805@users.noreply.github.com>
2024-10-30 23:20:57 +0530

    Update newan0805_bio.js

--- newan0805_bio.js
+++ newan0805_bio.js
@@ -1,16 +1,26 @@
 {
-  "name": "",
-  "bio": "test",
+  "name": "Newan Kumanayaka",
+  "bio": "Associate Software Engineer | Bachelor of Science in Network Systems Engineering",
   "image": "https://raw.githubusercontent.com/newan0805/newan0805_bio/refs/heads/main/1640686871560.jpeg",
-  "description": "test",
+  "description": "As an Associate Software Engineer at E Gravity Solutions, I immerse myself in developing innovative software solutions that align with our organizational objectives. With an ongoing Bachelor of Science in Network Systems Engineering at the University of Sunderland, my academic insights enhance our team's capabilities in tackling complex technological challenges. Leadership and robotics are pivotal in our pursuit of excellence; these skills, coupled with a foundation in software development, enable us to deliver high-quality projects. Our collaborative approach, grounded in effective communication, has been instrumental in driving project success and fostering a culture of innovation within our team.",
   "social": [
+    {
+      "platform": "Gmail",
+      "url": "mailto:newan0805@gmail.com",
+      "icon": "FaGmail"
+    },
+    {
+      "platform": "Linkedin",
+      "url": "https://www.linkedin.com/in/newan-kumanayaka-75bb871ba/",
+      "icon": "FaLinkedin"
+    },
     {
       "platform": "GitHub",
       "url": "https://github.com/newan0805",
       "icon": "FaGithub"
-    }
+    },
   ],
   "projects": {
-    "description": "test"
+    "description": "This project showcases my skills and expertise in software development, particularly in network systems engineering and cybersecurity. It features a clean and responsive design, highlighting my professional journey, projects, and contact information. The site pulls data dynamically from an API, ensuring that content is always up to date. Users can easily navigate through sections like About Me, Projects, and Social Links, making it a comprehensive overview of my work and experience."
   }
 }
