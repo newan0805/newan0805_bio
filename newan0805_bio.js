de0b80cfd852b901c328afbca6e8f9dde1e0ec1b
Newan Kumanayaka <94077976+newan0805@users.noreply.github.com>
2024-10-30 23:06:13 +0530

    Update newan0805_bio.js

--- newan0805_bio.js
+++ newan0805_bio.js
@@ -5,29 +5,9 @@
   "description": "test",
   "social": [
     {
-      "platform": "linkedin",
-      "url": "",
-      "icon": "https://img.icons8.com/ios-filled/50/linkedin.png"
-    },
-    {
-      "platform": "gmail",
-      "url": "",
-      "icon": "https://img.icons8.com/ios-filled/50/gmail-new.png"
-    },
-    {
-      "platform": "instagram",
-      "url": "",
-      "icon": "https://img.icons8.com/windows/32/instagram-new.png"
-    },
-    {
-      "platform": "medium",
-      "url": "",
-      "icon": "https://img.icons8.com/sf-regular-filled/48/medium-logo.png"
-    },
-    {
-      "platform": "github",
-      "url": "",
-      "icon": "https://img.icons8.com/ios-glyphs/30/github.png"
+      "platform": "GitHub",
+      "url": "https://github.com/newan0805",
+      "icon": "FaGithub"
     }
   ],
   "projects": {
